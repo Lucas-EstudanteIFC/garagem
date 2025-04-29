69a8b28dfd7ac7e77462e82b5ba82ad14a9214b9
Lucas-EstudanteIFC <alunolucasmartins62@gmail.com>
2025-04-29 08:58:57 -0300

    feat: Criação da tabela Veículo

---
+++ core/models/veiculo.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+from django.db.models.fields.related import ForeignKey
+
+
+from core.models import Acessorio, Cor, Modelo
+
+class Veiculo(models.Model):
+    ano = models.IntegerField(default=0, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True )
+    modelo = ForeignKey(Modelo, on_delete=models.PROTECT, related_name="carros")
+    cor = ForeignKey(Cor, on_delete=models.PROTECT, related_name="carros")
+    acessorios = models.ManyToManyField(Acessorio, related_name="carros", blank=True)
+
+    def __str__ (self):
+        return f"{self.modelo.nome} {self.cor.nome} {self.ano} ({self.id})"
