5e791ab3ca38158fb7928f3d4390f39626588304
Lucas-EstudanteIFC <alunolucasmartins62@gmail.com>
2025-05-09 15:57:32 -0300

    fix: Correção com o professor

--- core/models/modelo.py
+++ core/models/modelo.py
@@ -1,11 +1,9 @@
-from pickle import TRUE
 from django.db import models
 
-from django.template.defaultfilters import upper
 
 class Modelo(models.Model):
     nome = models.CharField(max_length=80)
     marca = models.CharField(max_length=80, blank=True, null=True)
 
     def __str__(self):
-        return f"{upper(self.nome)}/{upper(self.marca)} ({self.id})"
+        return f"{self.nome.upper()}/{self.marca.upper()} ({self.id})"
--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,13 +1,10 @@
-
 from django.db import models
-from django.db.models.fields.related import ForeignKey
-
 
 from core.models import Acessorio, Cor, Modelo
 
 class Veiculo(models.Model):
-    modelo = ForeignKey(Modelo, on_delete=models.PROTECT, related_name="carros")
-    cor = ForeignKey(Cor, on_delete=models.PROTECT, related_name="carros")
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="carros")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="carros")
     acessorios = models.ManyToManyField(Acessorio, related_name="carros", blank=True)
     ano = models.IntegerField(default=0, null=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True )
